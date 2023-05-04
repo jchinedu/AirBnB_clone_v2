cc075115bba6561789946265ffa71315c8906dbe
jchinedu <jchinedu080@gmail.com>
2023-05-04 11:33:09 +0100

    git

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from os import path
 
 
-env.hosts = ['18.209.20.255', '34.73.76.135']
+env.hosts = ['54.237.43.213', '3.95.32.123']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
